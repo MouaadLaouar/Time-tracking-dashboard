ab22804ae6d4f16502c03cb6ec1f360c441c0b6b
laouar mouaad <laouarmohamedmouad@gmail.com>
2021-09-16 19:00:41 +0100

    i finish the project but there some bugs i need to solved latter

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,183 @@
-console.log("i'am still coocking");
+const data = [
+    {
+      "title": "Work",
+      "timeframes": {
+        "daily": {
+          "current": 5,
+          "previous": 7
+        },
+        "weekly": {
+          "current": 32,
+          "previous": 36
+        },
+        "monthly": {
+          "current": 103,
+          "previous": 128
+        }
+      }
+    },
+    {
+      "title": "Play",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 2
+        },
+        "weekly": {
+          "current": 10,
+          "previous": 8
+        },
+        "monthly": {
+          "current": 23,
+          "previous": 29
+        }
+      }
+    },
+    {
+      "title": "Study",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 7
+        },
+        "monthly": {
+          "current": 13,
+          "previous": 19
+        }
+      }
+    },
+    {
+      "title": "Exercise",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 5
+        },
+        "monthly": {
+          "current": 11,
+          "previous": 18
+        }
+      }
+    },
+    {
+      "title": "Social",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 3
+        },
+        "weekly": {
+          "current": 5,
+          "previous": 10
+        },
+        "monthly": {
+          "current": 21,
+          "previous": 23
+        }
+      }
+    },
+    {
+      "title": "Self Care",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 2,
+          "previous": 2
+        },
+        "monthly": {
+          "current": 7,
+          "previous": 11
+        }
+      }
+    }
+  ]
+
+const daily_btn = document.getElementById("daily-btn");
+const weekly_btn = document.getElementById("weekly-btn");
+const monthly_btn = document.getElementById("monthly-btn");
+
+//function
+
+const daily = () => {
+  //work
+  document.getElementById("cureentwork").innerHTML = data[0].timeframes.daily.current;
+  document.getElementById("previouswork").innerHTML = data[0].timeframes.daily.previous;
+  //Play
+  document.getElementById("cureentPlay").innerHTML = data[1].timeframes.daily.current;
+  document.getElementById("previousPlay").innerHTML = data[1].timeframes.daily.previous;
+  //Study 
+  document.getElementById("cureentStudy").innerHTML = data[2].timeframes.daily.current;
+  document.getElementById("previousStudy").innerHTML = data[2].timeframes.daily.previous;
+  //Exercise
+  document.getElementById("cureentExercise").innerHTML = data[3].timeframes.daily.current;
+  document.getElementById("previousExercise").innerHTML = data[3].timeframes.daily.previous;
+  //Social
+  document.getElementById("cureentSocial").innerHTML = data[4].timeframes.daily.current;
+  document.getElementById("previousSocial").innerHTML = data[4].timeframes.daily.previous;
+  //Self Care
+  document.getElementById("cureentSelf").innerHTML = data[5].timeframes.daily.current;
+  document.getElementById("previousSelf").innerHTML = data[5].timeframes.daily.previous;
+}
+
+const weekly = () => {
+  //work
+  document.getElementById("cureentwork").innerHTML = data[0].timeframes.weekly.current;
+  document.getElementById("previouswork").innerHTML = data[0].timeframes.weekly.previous;
+  //Play
+  document.getElementById("cureentPlay").innerHTML = data[1].timeframes.weekly.current;
+  document.getElementById("previousPlay").innerHTML = data[1].timeframes.weekly.previous;
+  //Study 
+  document.getElementById("cureentStudy").innerHTML = data[2].timeframes.weekly.current;
+  document.getElementById("previousStudy").innerHTML = data[2].timeframes.weekly.previous;
+  //Exercise
+  document.getElementById("cureentExercise").innerHTML = data[3].timeframes.weekly.current;
+  document.getElementById("previousExercise").innerHTML = data[3].timeframes.weekly.previous;
+  //Social
+  document.getElementById("cureentSocial").innerHTML = data[4].timeframes.weekly.current;
+  document.getElementById("previousSocial").innerHTML = data[4].timeframes.weekly.previous;
+  //Self Care
+  document.getElementById("cureentSelf").innerHTML = data[5].timeframes.weekly.current;
+  document.getElementById("previousSelf").innerHTML = data[5].timeframes.weekly.previous;
+}
+
+const monthly = () => {
+  //work
+  document.getElementById("cureentwork").innerHTML = data[0].timeframes.monthly.current;
+  document.getElementById("previouswork").innerHTML = data[0].timeframes.monthly.previous;
+  //Play
+  document.getElementById("cureentPlay").innerHTML = data[1].timeframes.monthly.current;
+  document.getElementById("previousPlay").innerHTML = data[1].timeframes.monthly.previous;
+  //Study 
+  document.getElementById("cureentStudy").innerHTML = data[2].timeframes.monthly.current;
+  document.getElementById("previousStudy").innerHTML = data[2].timeframes.monthly.previous;
+  //Exercise
+  document.getElementById("cureentExercise").innerHTML = data[3].timeframes.monthly.current;
+  document.getElementById("previousExercise").innerHTML = data[3].timeframes.monthly.previous;
+  //Social
+  document.getElementById("cureentSocial").innerHTML = data[4].timeframes.monthly.current;
+  document.getElementById("previousSocial").innerHTML = data[4].timeframes.monthly.previous;
+  //Self Care
+  document.getElementById("cureentSelf").innerHTML = data[5].timeframes.monthly.current;
+  document.getElementById("previousSelf").innerHTML = data[5].timeframes.monthly.previous;
+}
+
+daily_btn.addEventListener('click', daily);
+weekly_btn.addEventListener('click', weekly);
+monthly_btn.addEventListener('click', monthly);
+
+
+
+
+
+
